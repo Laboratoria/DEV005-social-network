beed64414af8b2fa15d19475cff1736404ffec0e
T-Barrios <124935961+T-Barrios@users.noreply.github.com>
2023-03-31 12:37:34 -0300

    Firebase Instalado

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,24 @@
 // aqui exportaras las funciones que necesites
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAF-3mjemO44AIf70D7JQ8gVwQGMdS67ag",
+  authDomain: "social-network-05-e6e95.firebaseapp.com",
+  projectId: "social-network-05-e6e95",
+  storageBucket: "social-network-05-e6e95.appspot.com",
+  messagingSenderId: "624975340622",
+  appId: "1:624975340622:web:fa39eef9c17e33ff4c4f1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export const myFunction = () => {
   // aqui tu codigo
-  console.log('Hola mundo!');
+  console.log('Hola mundo!', app);
 };

8fad52d6e226dbec76b0b98cc5e1fab5c47ad44b
iraprojects <kristenirazu@gmail.com>
2023-03-31 13:00:28 -0600

    se añadio la carpeta de templates junto con login.js y register.js

---
+++ src/templates/register.js
@@ -0,0 +1,18 @@
+export function Register() {
+  return `<h2>Registrate</h2>
+    <section id="passAndEm">
+    <label for="email" class="cEmail">Correo electronico:
+    <input  type="email" id="email" ></label>
+    <label for="user" class="cUser">Usuaria:
+    <input  type="text" id="user" ></label>
+    <label for="password" class="cPassword">Contaseña:
+    <input type="password" id="password"></label>
+    </section>
+    
+    <section id="register">
+    <button id="btn-register">Registrate</button>
+    <p>¿Ya tienes cuenta? </p>
+    <a href="">Inicia sesion</a>
+    <a href=""><img src="./img/2000px-Google_G_Logo.svg_.png" alt="google logo" id="google-Logo" width="30px"></a>
+    </section>`;
+}

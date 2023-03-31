b124a8befc5e3a30434d7962726b6bd97a6eb301
SandraMTJ <sandra.tabares.j@gmail.com>
2023-03-31 13:04:31 -0500

    Se agrega vista de registro

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,22 +1,20 @@
-export default  {
-    404: {
-        template: "/templates/404.html",
-        title: "404",
-    },
-    "/": {
-        template: "/templates/login/login.html",
-        title: "Mascoteando",
-        js: [
-            "/templates/login/login.js",
-        ]
-    },
-    "/home": {
-        template: "/templates/home/home.html",
-        title: "Mascoteando - Home",
-        js: [
-            "/templates/home/home.js",
-            "/templates/home/loing.js",
-        ]
-    },
+export default {
+  404: {
+    template: '/templates/404.html',
+    title: '404',
+  },
+  '/': {
+    template: '/templates/login/login.html',
+    title: 'Mascoteando',
+    js: [
+      '/templates/login/login.js',
+    ],
+  },
+  '/registro': {
+    template: '/templates/registro/registro.html',
+    title: 'Mascoteando - Registro',
+    js: [
+      '/templates/registro/registro.js',
+    ],
+  },
 };
-  

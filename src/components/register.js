ede7eca8c71af391834a8ca6db570f3e972d0163
sofiauldemia <sofy.torresv@gmail.com>
2023-04-04 12:53:08 -0500

    ADD:Navigation starter point

---
+++ src/components/register.js
@@ -0,0 +1,7 @@
+function register(){
+    const titulo = document.createElement('h2');
+    titulo.textContent = 'pagina de registro';
+
+    return titulo;
+}
+export default register;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { login } from './components/login.js';
+import register from './components/register.js';
 
 const root = document.getElementById('root');
-root.append(login());
+// root.append(register());
+const routes = [
+    { path:'/', component: login },
+    { path:'/register', component: register }
+];
+
+const defaultRoute = '/';
+function navigateTo(hash) {
+    const route = routes.find((routeFind) => routeFind.path === hash);
+  
+    if (route && route.component) {
+      window.history.pushState(
+        {},
+        route.path,
+        window.location.origin + route.path,
+      );
+      root.appendChild(route.component());
+    }
+    
+  }
+  navigateTo(window.location.pathname);

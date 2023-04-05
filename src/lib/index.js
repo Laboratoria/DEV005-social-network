460692d72ba5d146073878aab1086417fb6f8727
unknown <agudelomariana2503@gmail.com>
2023-04-05 16:06:37 -0500

    UPDATE: movement to the register labels

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,25 +1,40 @@
 import { handleLoginEmail } from './auth.js';
 
+// Función para mover etiquetas de formulario
+// Agrega la clase 'active' al label cuando el input tiene el foco
+// y la quita cuando el input pierde el foco y está vacío
+function labelMovement(inputs) {
+  inputs.forEach((input) => {
+    const label = document.querySelector(`label[for="${input.id}"]`);
+
+    input.addEventListener('click', () => {
+      label.classList.add('active');
+    });
+
+    input.addEventListener('blur', () => {
+      if (input.value === '') {
+        label.classList.remove('active');
+      }
+    });
+  });
+}
+
+// Obtener elementos del DOM
 const btnLogin = document.getElementById('btn-login');
+const inputsLogin = document.querySelectorAll('.inp-login');
+const inputsRegister = document.querySelectorAll('.inp-register');
 
-btnLogin.addEventListener('click', (event) => {
-  event.preventDefault();
-  handleLoginEmail();
-});
+// Verificar que elementos están cargados en el DOM
+document.addEventListener('DOMContentLoaded', () => {
+  // Si el botón de inicio de sesión existe, agregar evento click
+  if (btnLogin) {
+    btnLogin.addEventListener('click', (event) => {
+      event.preventDefault();
+      handleLoginEmail();
+    });
+  }
 
-// Mostrar o esconder labels
-const inputs = document.querySelectorAll('.inp-login');
-
-inputs.forEach((input) => {
-  const label = document.querySelector(`label[for="${input.id}"]`);
-
-  input.addEventListener('click', () => {
-    label.classList.add('active');
-  });
-
-  input.addEventListener('blur', () => {
-    if (input.value === '') {
-      label.classList.remove('active');
-    }
-  });
+  // Agregar movimiento de etiqueta a los inputs necesarios
+  labelMovement(inputsLogin);
+  labelMovement(inputsRegister);
 });

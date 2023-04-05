dc8a9d8eebea7ca27d841913571bb783864f894c
unknown <agudelomariana2503@gmail.com>
2023-04-05 14:11:34 -0500

    ADD: navigation routes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,39 @@
 import { login } from './components/login.js';
-import register from './components/register.js';
+import { register } from './components/register.js';
+import error from './components/error.js';
+import wall from './components/wall.js';
 
 const root = document.getElementById('root');
 // root.append(register());
 const routes = [
-    { path:'/', component: login },
-    { path:'/register', component: register }
+  { path: '/', component: login },
+  { path: '/register', component: register },
+  { path: '/error', component: error },
+  { path: '/wall', component: wall },
 ];
 
 const defaultRoute = '/';
-function navigateTo(hash) {
-    const route = routes.find((routeFind) => routeFind.path === hash);
-  
-    if (route && route.component) {
-      window.history.pushState(
-        {},
-        route.path,
-        window.location.origin + route.path,
-      );
-      root.appendChild(route.component());
+
+export function navigateTo(hash) {
+  const route = routes.find((routeFind) => routeFind.path === hash);
+
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
     }
-    
+    root.appendChild(route.component(navigateTo));
+  } else {
+    navigateTo('/error');
   }
+}
+
+window.onpopstate = () => {
   navigateTo(window.location.pathname);
+};
+
+navigateTo(window.location.pathname || defaultRoute);

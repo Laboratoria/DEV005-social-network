0669ebc8e6a3238634cc627e35199903fd962caf
unknown <agudelomariana2503@gmail.com>
2023-04-05 14:14:57 -0500

    ADD: import function loginEmailPassword

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,8 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import { loginEmailPassword } from '../src/lib/auth.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe('loginEmailPassword', () => {
+  it('Is a function', () => {
+    expect(typeof loginEmailPassword).toBe('function');
   });
 });

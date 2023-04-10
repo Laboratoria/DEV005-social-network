c1d4e07c23d52416ab34b666554603babe649a8a
moniglz <monie.gop@gmail.com>
2023-04-10 17:12:44 -0600

    ADD: auth for new account and UPDATE: register.js, provisionally comment input: name, user, confirm pass

---
+++ src/components/register.js
@@ -0,0 +1,48 @@
+import { newAccount } from '../lib/auth';
+
+function register(navigateTo) {
+  const sectionReg = document.createElement('section');
+  sectionReg.id = 'reg-account';
+  const titleReg = document.createElement('h2');
+  const inputEmail = document.createElement('input');
+  inputEmail.id = 'email';
+  inputEmail.placeholder = 'Escribe tu correo';
+  // const inputName = document.createElement('input');
+  // inputName.id = 'full-name';
+  // inputName.placeholder = 'Nombre completo';
+  // const inputUser = document.createElement('input');
+  // inputUser.id = 'nombre-usuario';
+  // inputUser.placeholder = 'Tu usuario';
+  const inputPass = document.createElement('input');
+  inputPass.id = 'contraseña';
+  inputPass.placeholder = 'Escribe tu contraseña';
+  // const inputConfirmPass = document.createElement('input');
+  // inputConfirmPass.id = 'confirmar-contra';
+  // inputConfirmPass.placeholder = 'Confirma tu contraseña';
+  const btnSend = document.createElement('button');
+  btnSend.id = 'enviar-info';
+
+  titleReg.textContent = '¡Regístrate!';
+  btnSend.textContent = 'Registrarme';
+
+  const btnReturnH = document.createElement('button');
+  btnReturnH.textContent = 'Return to home';
+  btnReturnH.addEventListener('click', () => {
+    navigateTo('/');
+  });
+
+  sectionReg.append(
+    titleReg,
+    inputEmail,
+    // inputName,
+    // inputUser,
+    inputPass,
+    // inputConfirmPass,
+    btnSend,
+    btnReturnH,
+  );
+  newAccount('example@mail.com', '12345');
+  return sectionReg;
+}
+
+export default register;

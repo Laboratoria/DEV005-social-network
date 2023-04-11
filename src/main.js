5cbbc5590e2fb1b1ea3a03b9173c55489a8a35da
abrakel <abravokeller@gmail.com>
2023-04-11 18:13:07 -0400

    Se realizó la estructura de la página home y se dio diseño segun prototipo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
-// Este es el punto de entrada de tu aplicacion
+import home from './components/home.js';
+import error from './components/error.js';
 
-import { myFunction } from './lib/index.js';
+/* myFunction(); */
+const root = document.getElementById('root');
 
-myFunction();
+const routes = [
+  { path: '/', component: home },
+  /* {path: '/login', component: login}, */
+  { path: '/error', component: error },
+];
+
+const defaultRoute = '/';
+
+function navigateTo(hash) {
+  const route = routes.find((routeFind) => routeFind.path === hash);
+  if (route && route.component) {
+    window.history.pushState({}, route.path, window.location.origin + route.path);
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    // appendChild para traer solo un elemento, y se usa append para mas de un elemento
+    root.appendChild(route.component(navigateTo));
+  } else {
+    navigateTo('/error');
+  }
+}
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
+};
+// pathname solo toma lo que va despues de slash en la ruta
+navigateTo(window.location.pathname || defaultRoute);

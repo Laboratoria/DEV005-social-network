6bc4e76aa57812a88d2b0811d5afcf8cd0b0d8a5
adrieli2307 <aearmijosq1104@gmail.com>
2023-04-11 14:13:16 -0500

    Formulario registro e implementacion de estilos

---
+++ src/componentes/registro.js
@@ -0,0 +1,63 @@
+/* eslint-disable max-len */
+function registro(navigateTo) {
+  const section = document.createElement('section');
+  /* Elementos */
+  const title = document.createElement('h2');
+
+  const img = document.createElement('img');
+  img.id = 'imagen';
+  img.src = '../Img/logo.jpg';
+
+  const nameLabel = document.createElement('label');
+  const name = document.createElement('input');
+  nameLabel.textContent = 'Name:';
+  nameLabel.setAttribute('for', 'name');
+  name.id = 'name';
+  name.placeholder = 'Write your name';
+
+  const lastnameLabel = document.createElement('label');
+  const lastname = document.createElement('input');
+  lastnameLabel.textContent = 'Last Name:';
+  lastnameLabel.setAttribute('for', 'lastname');
+  lastname.id = 'lastname';
+  lastname.placeholder = 'Write your last name';
+
+  const passwordLabel = document.createElement('label');
+  const password = document.createElement('input');
+  passwordLabel.textContent = 'Password:';
+  passwordLabel.setAttribute('for', 'password');
+  password.id = 'password';
+  password.placeholder = 'Enter a password';
+
+  const mailLabel = document.createElement('label');
+  const mail = document.createElement('input');
+  mailLabel.textContent = 'Email:';
+  mailLabel.setAttribute('for', 'mail');
+  mail.id = 'mail';
+  mail.placeholder = 'Write your email';
+
+  const addressLabel = document.createElement('label');
+  const address = document.createElement('input');
+  addressLabel.textContent = 'City:';
+  addressLabel.setAttribute('for', 'address');
+  address.id = 'address';
+  address.placeholder = 'Write your city';
+
+  const birthdayLabel = document.createElement('label');
+  const birthday = document.createElement('input');
+  birthdayLabel.textContent = 'Birthday:';
+  birthdayLabel.setAttribute('for', 'birthday');
+  birthday.id = 'birthday';
+  birthday.type = 'date';
+  birthday.placeholder = 'Write your birthday';
+
+  const register = document.createElement('button');
+  register.textContent = 'Registrarse';
+
+  title.textContent = 'Pet Registro';
+
+  section.append(title, nameLabel, name, lastnameLabel, lastname, passwordLabel, password, mailLabel, mail, addressLabel, address, birthdayLabel, birthday, register, img);
+  return section;
+}
+
+export default registro;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
-// Este es el punto de entrada de tu aplicacion
+import registro from './componentes/registro';
 
-import { myFunction } from './lib/index.js';
+const root = document.getElementById('root');
 
-myFunction();
+const routes = [
+  { path: '/', component: registro },
+];
+
+const defaultRoute = '/';
+
+function navigateTo(hash) {
+  const route = routes.find((routeFind) => routeFind.path === hash);
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    root.appendChild(route.component(navigateTo));
+  } else {
+    navigateTo('/error');
+  }
+}
+
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
+};
+
+navigateTo(window.location.pathname || defaultRoute);

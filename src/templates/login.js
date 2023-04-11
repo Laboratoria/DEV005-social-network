aebbc8e29bf05d00f058031ef500be605cd2d994
andrenavas <andreynanavas@gmail.com>
2023-04-11 11:32:26 -0500

    Arreglando responsive a la vista login

--- src/templates/login.js
+++ src/templates/login.js
@@ -8,35 +8,35 @@ import { navegacion } from '../main.js';
 // eslint-disable-next-line no-shadow
 export function login(navegacion) {
   const section = document.createElement('section');
-  if(sessionStorage.getItem("user")){
-      navegacion("/home")
-      return "<div></div>"
+  if(sessionStorage.getItem('user')){
+      navegacion('/home')
+      return '<div></div>'
     }
 
   const html = `
-  <section>
-  <div class="contenedorLogo">
-    <img class="imgLogo" src="img/logo.png" alt="logo">
-  </div>
-  </section>
-  <main>
-  <div class="contenedorIngreso">
-    <form class="contenedorInput">
-      <label>
-        <span>Correo electrónico</span>
-        <input type="email" autocomplete="off" name="email" class="email" id="email">
-      </label>
-      <label>
-        <span>Contraseña</span>
-        <input type="password" class="password" id="password">
-      </label>  
-      
-    </form>   
-      <button class="btnInicio" id="btnInicio">Iniciar sesión</button>
-      <button class="btnGoogle" id="btnGoogle"> <img class="logoGoogle" src="./img/logogoogle.png" alt="Logo Google"></img>Continuar con google</button>
-      <span class="textRegistro">Si no tienes cuenta <button class="btnRegistro" id="btnRegistro">Registrate</button></span>
-  </div>
-  </main>`;
+  <div class='contenedor'>
+    <div class="contenedorLogo">
+      <img class="imgLogo" src="img/logo.png" alt="logo">
+    </div>
+    <main>
+      <div class="contenedorIngreso">
+        <form class="contenedorInput">
+          <label>
+            <span>Correo electrónico</span>
+            <input type="email" autocomplete="off" name="email" class="email" id="email">
+          </label>
+          <label>
+            <span>Contraseña</span>
+            <input type="password" class="password" id="password">
+          </label>  
+          
+        </form>   
+        <button class="btnInicio" id="btnInicio">Iniciar sesión</button>
+        <button class="btnGoogle" id="btnGoogle"> <img class="logoGoogle" src="./img/logogoogle.png" alt="Logo Google"></img>Continuar con google</button>
+        <span class="textRegistro">Si no tienes cuenta <button class="btnRegistro" id="btnRegistro">Regístrate</button></span>
+      </div>
+    </main>
+  </div>`;
   
   section.innerHTML = html;
 
@@ -92,4 +92,6 @@ export function login(navegacion) {
   });
 
   return section;
+
+  
 }

2860c9f6dc6d0ddc6724ea306c184c5033d45be5
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-04-12 19:30:31 -0500

    Botones saltarines

--- src/templates/registerTemplate.js
+++ src/templates/registerTemplate.js
@@ -2,20 +2,21 @@ export const RegisterTemplate = `
   <h2>Crea tu cuenta</h2>
   <section id="container-register">
   <form id="signup">
-    <section id="passAndEm">
+    <section class="passAndEm">
     <label for="email" class="cEmail">Correo electrónico:
     <input  type="email" id="email" required ></label>
     <label for="user" class="cUser">Usuaria:
     <input  type="text" id="user" required ></label>
     <label for="password" class="cPassword">Contaseña:
     <input type="password" id="password" required></label>
+    <p id="errorMsj"></p>
     <button type="submit" id="btn-register">Registrate</button>
     </section>
     
     <section id="register">
     
     <p>¿Ya tienes cuenta? </p>
-    <a href="">Inicia sesion</a>
+    <button id="linkLogin">Inicia sesión</button>
     </section>
   </form>
   </section>

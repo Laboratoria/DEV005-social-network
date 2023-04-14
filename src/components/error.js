e3c6403cc4909c8100526b968f44aca7a31ced8a
unknown <caroraquelq@gmail.com>
2023-04-14 18:19:31 -0500

    Creacion de rutas

---
+++ src/components/error.js
@@ -0,0 +1,9 @@
+function error() {
+  const section = document.createElement('section');
+  const title = document.createElement('h1');
+  title.textContent = 'Error 404 nai';
+  section.append(title);
+  return section;
+}
+
+export default error;

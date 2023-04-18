a3436c413df7bfc884bdd7739607d0f8df4c4444
Claudia <holaclaudiia@gmail.com>
2023-04-18 16:57:18 -0500

    modifica la home

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,12 +1,13 @@
 function home() {
-  const postForm = document.createElement('div');
+  const postForm = document.createElement('section');
   postForm.classList.add('home');
   postForm.innerHTML = `
-    <img src='./lib/img/fondoHome.png' class= 'logoHome'>
+    <img src='./lib/img/logoHome.png' class= 'logoHome'>
     <form>
     <textarea class="postForm" placeholder= "Comparte tu negocio..."></textarea>
     <button type= "submit" class= "post" > Publicar </button>
-    </form>`;
+    </form>
+    <button type= "submit" class= "goOut" > Salir </button>`;
   return postForm;
 }
 

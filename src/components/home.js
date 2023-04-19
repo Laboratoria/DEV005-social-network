94320d924581c887204dd9a9aed86546a7da9e44
Karen Huamán <karmahucents@gmail.com>
2023-04-19 13:09:07 -0500

    update: resolviendo errores de importación

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,6 @@
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-} from 'firebase/auth';
-import { auth, provider } from '../lib/firebaseConfig.js';
-import { loginWithGithub } from '../lib/loginConfig.js';
+import { loginWithGithub, loginWithGoogle } from '../lib/loginConfig.js';
 
-function home(navigateTo) {
+const home = (navigateTo) => {
   const homediv = document.createElement('div');
   homediv.className = 'homediv';
   const imghome = document.createElement('div');
@@ -29,6 +24,9 @@ function home(navigateTo) {
   // ? botón de twitter
   const buttontwitter = document.createElement('button');
   buttontwitter.className = 'buttontwitter';
+  buttontwitter.addEventListener('click', () => {
+    alert('Estamos trabajando en ello');
+  });
   const icontwitter = document.createElement('div');
   icontwitter.className = 'icontwitter';
   // ? botón de github
@@ -53,16 +51,9 @@ function home(navigateTo) {
     navigateTo('/login');
   });
   buttongoogle.textContent = 'Continuar con Google';
-  buttongoogle.addEventListener('click', async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const credentials = await signInWithPopup(auth, provider);
-      console.log(credentials);
-      console.log('sign in with google');
-      navigateTo('/muro');
-    } catch (error) {
-      console.log(error.message);
-    }
+  buttongoogle.addEventListener('click', () => {
+    loginWithGoogle();
+    navigateTo('/muro');
   });
   buttontwitter.textContent = 'Continuar con Twitter';
   buttongithub.textContent = 'Continuar con Github';
@@ -80,6 +71,6 @@ function home(navigateTo) {
   buttontwitter.appendChild(icontwitter);
   buttongithub.append(icongithub);
   return homediv;
-}
+};
 
 export default home;

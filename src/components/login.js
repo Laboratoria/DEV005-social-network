d145213a27612af3dbd72ef3403e95fc7c21060d
Maria T <flormaria_2424@outlook.com>
2023-04-18 22:22:24 -0500

    login para presentacion de demo 19'04

--- src/components/login.js
+++ src/components/login.js
@@ -1,20 +1,37 @@
-function login() {
+function login(navigateTo) {
   const root = document.getElementById('root');
   const section = document.createElement('section');
   section.classList.add('contenedor');
   const contendorLr = document.createElement('div');
   contendorLr.classList.add('contenedorLr');
+
+  /* izquierda   */
+  const divLeft = document.createElement('div');
+  divLeft.classList.add('divLeft');
+  divLeft.id = ('divLeft');
+  const imgLogin = document.createElement('img');
+  const divSocialNetword = document.createElement('div');
+  divSocialNetword.classList.add('divSocialNetword');
+  const containerDiv = document.createElement('div');
+  containerDiv.classList.add('containerDiv');
+  /* derecha   */
+
   const divRight = document.createElement('div');
-  divRight.classList.add('secctionDiv');
-  divRight.id = ('divRight');
+  divRight.classList.add('divRaight');
   const loginTitle = document.createElement('h2');
+  loginTitle.classList.add('loginTitle');
   const userName = document.createElement('span');
   const userPassword = document.createElement('span');
   const userAsk = document.createElement('span');
+  userAsk.classList.add('askP');
   const inputUser = document.createElement('input');
+  inputUser.classList.add('inputClassLogin');
   const inputPassword = document.createElement('input');
+  inputPassword.classList.add('inputClassLogin');
   const buttonLogin = document.createElement('button');
+  buttonLogin.classList.add('buttonClassLogin');
   const buttonRegister = document.createElement('button');
+  buttonRegister.classList.add('buttonClassLogin');
   const divUser = document.createElement('div');
   divUser.classList.add('loginDiv');
   const divPassword = document.createElement('div');
@@ -25,57 +42,40 @@ function login() {
   divLogin.classList.add('loginDiv');
   const divAsk = document.createElement('div');
   divAsk.classList.add('loginDiv');
-  /* izquierda   */
-  const divLeft = document.createElement('div');
-  divLeft.classList.add('secctionDiv');
-  divLeft.id = ('divLeft');
-  const imgLogin = document.createElement('img');
-  const nameSocialNetwrk = document.createElement('span');
-  nameSocialNetwrk.classList.add('spanSocialNetwrk');
-  const containerDiv = document.createElement('div');
-  containerDiv.classList.add('containerDiv');
 
   root.append(section);
   section.append(contendorLr);
-  contendorLr.append(divRight, divLeft);
+
+  contendorLr.append(divLeft, divRight);
   loginTitle.textContent = 'LOGIN';
-  divRight.append(loginTitle);
-  divRight.append(containerDiv);
-  containerDiv.append(divUser);
-  containerDiv.append(divPassword);
-  containerDiv.append(divAsk);
-  containerDiv.append(divRegister);
-  containerDiv.append(divLogin);
+  divRight.append(loginTitle, containerDiv);
+  containerDiv.append(divUser, divPassword, divLogin, divAsk, divRegister);
 
-  divRight.append(divUser);
   userName.textContent = 'USER NAME';
-  divUser.append(userName);
-  divUser.append(inputUser);
+  divUser.append(userName, inputUser);
 
-  divRight.append(divPassword);
   userPassword.textContent = 'PASSWORD';
-  divPassword.append(userPassword);
-  divPassword.append(inputPassword);
+  divPassword.append(userPassword, inputPassword);
 
-  divRight.append(divLogin);
   buttonLogin.textContent = 'LOGIN';
   divLogin.append(buttonLogin);
 
-  divRight.append(divAsk);
   userAsk.textContent = 'YOU DONT HAVE USERNAME AND PASSWORD ';
   divAsk.append(userAsk);
 
-  divRight.append(divRegister);
   buttonRegister.textContent = 'REGISTER';
   divRegister.append(buttonRegister);
 
-  nameSocialNetwrk.textContent = 'SENIOR FACE';
-  divLeft.append(nameSocialNetwrk);
+  divSocialNetword.textContent = 'SENIOR FACE';
+  divLeft.append(divSocialNetword);
 
   imgLogin.src = '../components/img/seniorFaceLogin.png';
   imgLogin.classList.add('imgLogin');
-  imgLogin.alt = 'Es una imagen de una hija que muestra una aplicacion a su padre';
+  imgLogin.alt = 'Es una hija que muestra una aplicacion a su padre en un movil';
   divLeft.append(imgLogin);
+
+  // creacion de eventos
+  buttonRegister.addEventListener('click', () => { navigateTo('/register'); });
 
   return section;
 }

787e4951a984637cb987eb48facef7f02dcd0fe4
Belen Neira <belen.neira05@gmail.com>
2023-04-20 13:24:22 -0400

    probando test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,16 @@
 // import { navigateTo } from '../src/main.js';
 import { login } from '../src/Components/login.js';
 
-// jest.mock('../test/__mock__/main.js');
+// jest.mock('../src/lib/firebase.js');
 const navigateTo = jest.fn();
 
 // Función: Inicia Sesión
 describe('login', () => {
-  it('Si el usuario ingresa correctamente su gmail y su contraseña debería ir a home', () => {
+  /* it('Si el usuario ingresa correctamente su gmail y su contraseña debería ir a home', () => {
     const myHtml = login(navigateTo);
     myHtml.querySelector('.getInt').click();
     expect(navigateTo).toHaveBeenCalledWith('/emprende');
-  });
+  }); */
   it('Si el usuario no le da click a getIn, no debe cambiar de ruta', () => {
     login(navigateTo);
     expect(navigateTo).not.toHaveBeenCalledWith();

81c4892b4c461db49f53a72852b132559092c2da
iraprojects <kristenirazu@gmail.com>
2023-04-21 12:33:13 -0600

    funcion editar posts

--- src/DOM/Wall.js
+++ src/DOM/Wall.js
@@ -1,4 +1,7 @@
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+/* eslint-disable no-console */
+import {
+  collection, addDoc, getDocs, doc, getDoc, updateDoc,
+} from 'firebase/firestore';
 import { signOut, onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from '../lib/index.js';
 import { WallTemplate } from '../templates/wallTemplate.js';
@@ -10,25 +13,75 @@ export const Wall = (onNavigate) => {
   div.innerHTML = WallTemplate;
   const errorMsj = div.querySelector('#errorMsj');
   const divPost = div.querySelector('.posts');
+  const iPost = div.querySelector('#iPost');
+  let editStatus = false;
+  let idPost = '';
+
+  const getDocument = () => getDocs(collection(db, 'posts'));
+  const getPost = (id) => getDoc(doc(db, 'posts', id));
+  const updatePost = (id, newField) => updateDoc(doc(db, 'posts', id), newField);
+
+  const checkUser = (data) => {
+    const user = data.data().Author;
+    const userEmail = auth.currentUser.email;
+    if (user === userEmail) {
+      return true;
+    }
+    return false;
+  };
+
+  const editPost = (content) => {
+    const btnEdit = content.querySelectorAll('#btn-edit');
+    btnEdit.forEach((element) => element.addEventListener('click', async (e) => {
+      const docu = await getPost(e.target.dataset.id);
+      /* const user = docu.data().Author;
+      const userEmail = auth.currentUser.email; */
+      if (checkUser(docu)) {
+        console.log('El user ES autor del post');
+        iPost.value = docu.data().Post;
+        idPost = docu.id;
+        editStatus = true;
+      } else {
+        console.log('el user No es el autor del post');
+        iPost.value = '';
+        editStatus = false;
+      }
+    }));
+  };
 
   const showPost = (data) => {
     if (data.length) {
       let html = '';
-      data.forEach((doc) => {
-        const post = doc.data();
-        const li = `<li class='liPost' >${post.Post}</li>`;
+      data.forEach((docu) => {
+        const post = docu.data();
+        let li = '';
+        if (checkUser(docu)) {
+          li = `
+          <li class='liPost' >
+            ${post.Post}
+            <buttom class="btn-class" id="btn-edit" data-id="${docu.id}">Editar</buttom>
+          </li>
+          `;
+        } else {
+          li = `
+          <li class='liPost' >
+            ${post.Post}
+          </li>
+          `;
+        }
         html += li;
       });
       divPost.innerHTML = html;
     } else {
       errorMsj.innerHTML = 'No hay posts';
     }
+    editPost(div);
   };
 
   onAuthStateChanged(auth, async (user) => {
     if (user) {
-      const getPost = await getDocs(collection(db, 'posts'));
-      showPost(getPost.docs);
+      const getAllPosts = await getDocument();
+      showPost(getAllPosts.docs);
     } else {
       console.log('error post');
     }
@@ -38,7 +91,7 @@ export const Wall = (onNavigate) => {
     try {
       const docRef = await addDoc(collection(db, 'posts'), {
         Post: contenido,
-        /* fecha: firebase.firestore.FieldValue.serverTimestamp(), */
+        Author: auth.currentUser.email,
       });
       console.log('Document written with ID: ', docRef.id);
     } catch (e) {
@@ -47,20 +100,23 @@ export const Wall = (onNavigate) => {
   };
 
   const btnPost = div.querySelector('#btn-post');
-  const iPost = div.querySelector('#iPost');
   const btnOut = div.querySelector('#btn-out');
+  // const currentUser = auth.currentUser;
   btnPost.addEventListener('click', () => {
     const contenido = iPost.value.trim();
-    if (contenido !== '') {
+    if (editStatus) {
+      console.log('editando');
+      updatePost(idPost, { Post: iPost.value, Author: auth.currentUser.email });
+      editStatus = false;
+    } else if (contenido !== '') {
       crearPost(contenido);
-      iPost.value = '';
     }
+    iPost.value = '';
   });
 
   btnOut.addEventListener('click', async () => {
     await signOut(auth);
     onNavigate('/');
   });
-
   return div;
 };
--- src/templates/wallTemplate.js
+++ src/templates/wallTemplate.js
@@ -8,7 +8,11 @@ export const WallTemplate = `
     <label for="text" class="cPost">
       <textarea id="iPost" name="textarea" rows="10" cols="50" placeholder="Cuentanos sobre tu viaje..."></textarea>
     </label>
-    <button id="btn-post" class="publish">Publicar</button>
+    <button class="btn-class" id="btn-post">Publicar</button>
+    
     <p id='errorMsj' class='wallErrorMsj'></p>
     <div class="posts"></div>
 </div>`;
+
+/* <buttom class="btn-class" id="btn-edit">Editar</buttom>
+Esto se debe crear dinamicamente en el dom */

b1db8dbc3379aacd5915f555b34be08a3df31479
Claudia <holaclaudiia@gmail.com>
2023-04-21 14:01:58 -0500

    guarda acciones del test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,7 @@
 // import { navigateTo } from '../src/main.js';
 import { login } from '../src/Components/login.js';
+import { signIn } from '../src/lib/auth.js';
+
 /* import { signIn } from '../src/lib/auth.js'; */
 
 // jest.mock('../src/lib/firebase.js');
@@ -10,21 +12,23 @@ describe('login', () => {
   test('is a function ', () => {
     expect(typeof login).toBe('function');
   });
-  it('Si el usuario le da click a getIn, debe cambiar de ruta', () => {
+  // it('Si el usuario le da click a getIn, debe cambiar de ruta', () => {
+  //   const myHtml = login(navigateTo);
+  //   myHtml.querySelector('.formInteraction').submit();
+  //   expect(navigateTo).toHaveBeenCalledWith('/emprende');
+  // });
+  it('Si el usuario ingresa correctamente su gmail y su contraseña debería ir a home', () => {
+    // jest.spyOn(signIn).mockImplementation(navigateTo('/emprende'));
     const myHtml = login(navigateTo);
-    myHtml.querySelector('.formInteraction').submit();
-    expect(navigateTo).toHaveBeenCalledWith('/emprende');
-  });
-  /* it('Si el usuario ingresa correctamente su gmail y su contraseña debería ir a home', () => {
-    const myHtml = login(navigateTo);
+    // const myHtml = document.createElement('form');
+    // myHtml.append(signIn());
     const email = myHtml.querySelector('.inputEmail');
     const password = myHtml.querySelector('.inputPassword');
     email.value = 'amigas@gmail.com';
     password.value = '123456';
-    const button = myHtml.querySelector('.formInteraction');
-    button.submit();
-    expect(navigateTo).toHaveBeenCalledWith('/emprende');
-  }); */
+    myHtml.querySelector('.formInteraction').submit();
+    expect(navigateTo).toHaveBeenalledWith('/emprende');
+  });
   it('Si el usuario no le da click a getIn, no debe cambiar de ruta', () => {
     login(navigateTo);
     expect(navigateTo).not.toHaveBeenCalledWith();

bd20fa2bfd9c7b3d098922cd60363c13835ed031
Claudia <holaclaudiia@gmail.com>
2023-04-25 13:43:06 -0500

    Actualiza cambios

--- src/Components/mistake.js
+++ src/Components/mistake.js
@@ -1,21 +1,23 @@
-/* eslint-disable no-unused-expressions */
-// import { currentUserNow } from '../lib/auth.js';
+/* eslint-disable import/named */
+
+import { currentUserNow } from '../lib/auth.js';
 
 // pantalla - error
-function mistake() {
+function mistake(navigateTo) {
   const bug = document.createElement('div');
   bug.innerHTML = `<section class="errorSection"><img class="imgBug" src="../lib/img/error404.png">
   <h2 class="text"> Error 404: página no encontrada.</h2>
   <botton class="initButton"> <u>volver al Inicio</u> <botton>
   </section>`;
 
-  // const keep = bug.querySelector('.initButton');
-  // keep.addEventListener('click', () => {
-  //   const user = currentUserNow();
-  //   console.log(user);
-  //   user ? navigateTo('/emprende') : navigateTo('/');
-  // });
-  // return bug;
+  const keep = bug.querySelector('.initButton');
+  keep.addEventListener('click', () => {
+    const user = currentUserNow();
+    console.log(user);
+    // eslint-disable-next-line no-unused-expressions
+    user ? navigateTo('/emprende') : navigateTo('/');
+  });
+  return bug;
 }
 
 export {

e0db4efdbdcef574c39bc1ac49342a01241d1527
Ericka Gonzales <gonzaleslop03@gmail.com>
2023-04-26 22:33:22 -0500

    ADD: Accediendo al google API

--- src/firebase/configuracion.js
+++ src/firebase/configuracion.js
@@ -1,6 +1,6 @@
 // Importando las funciones necesarias de los SDK de Firebase que queremos utilizar en nuestro proyecto.
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Configurando los datos necesarios de nuestro proyecto de Firebase.
@@ -17,5 +17,24 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Inicializando la autenticación
 const auth = getAuth(app);
+signInWithPopup(auth, provider)
+    .then((result) => {
+    // Accediendo al google API
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // La información del usuario registrado
+        const user = result.user;
+    })
+    // Manejar los errores aquí
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // El correo del usuario
+        const email = error.customData.email;
+        // El tipo de autentificación que usó el usuario
+        const credential = GoogleAuthProvider.credentialFromError(error);
+    });
 // Obteniendo la instancia de Firestore para comenzar a utilizar la base de datos en nuestra aplicación web
 const database = getFirestore();
+// Obteniendo la instancia para autenticación google
+const provider = new GoogleAuthProvider(app);

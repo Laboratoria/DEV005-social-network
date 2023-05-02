f05642a64fee74e01c858c8aa00fe7f0724463d5
moniglz <monie.gop@gmail.com>
2023-05-01 20:32:26 -0600

    UPDATE styles for wall in desktop view

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -16,7 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
--- src/lib/post.js
+++ src/lib/post.js
@@ -1,21 +1,32 @@
-/* eslint-disable no-unused-vars */
 import { collection, addDoc, doc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { auth, db, app } from './firebase.js';
 
 // Add a new document with a generated id.
-export const addPostToFirestore = async (texto, user) => {
+export const addPostToFirestore = async (text, user) => {
   console.log('se añade');
-try{
-   const docRef = await addDoc(collection(db, 'posts'), {
-      texto,
-      user: 'user@user.com',
+  try{
+    const docRef = await addDoc(collection(db, 'posts'), {
+      text,
+      user: {
+        uid: user.uid, 
+        email: user.email,
+      }
     });
     console.log('Document written with ID: ', docRef.id);
-}
+  }
   catch{err => {
-  console.log('Error', err);
-}}
+    console.log('Error', err);
+  }}
 };
 
-
+export const deleteFirestorePost = async () => {
+  const idRef = doc(collection(db, 'posts'));
+  console.log('funciona deleteFirestorePost');
+  try {
+    const deletePost = await deleteDoc(doc(db, 'posts', idRef.id));
+    console.log('se borró el post ' + deletePost);
+  } catch{err => {
+    console.log('Error', err);
+  }}
+}

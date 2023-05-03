8b04300b8d279294b513ba4f42b862d58a4b0169
moniglz <monie.gop@gmail.com>
2023-05-03 12:35:31 -0600

    UPDATE styles for wall in tablet view, in progress, UPDATE wall.js, add button edit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,4 +18,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);

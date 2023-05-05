195b1eac76897dda7a2d72c9328387c848ac3295
Ericka Gonzales <gonzaleslop03@gmail.com>
2023-05-05 15:34:05 -0500

    ADD: Puliendo el estilo en loginBtn

--- test/loginLogic.spec.js
+++ test/loginLogic.spec.js
@@ -2,20 +2,22 @@
  * @jest-8environment ./src/test/my-custom-environment
  */
 import { loginLogic } from "../src/lib/loginLogic.js";
+// import * as configuracion from "../src/firebase/configuracion.js";
+import { login } from "../src/pages/login.js";
 
 describe("loginLogic", () => {
     test("should be a function", () => {
         expect(typeof loginLogic).toBe("function");
     });
+    // flujo - en el primer test traer cuno de los elementos/existencia, validaciones de texto
     test("after click the user navigates to /home", () => {
-        const domManipulation = document.createElement("section");
-        const findAndNavigate = jest.fn();
-        domManipulation.append((loginLogic(findAndNavigate)));
-        const loginBtn = domManipulation.querySelector("#loginBtn");
-        loginBtn.click();
-        expect(findAndNavigate).toBeCalledTimes("/home");
-    });
-    test("login button with promises", () => {
-
+        const findRouteAndNavigate = jest.fn();
+        const DOM = login(findRouteAndNavigate);
+        // const domManipulation = document.createElement("section");
+        // const findAndNavigate = jest.fn();
+        // domManipulation.append((loginLogic(findAndNavigate)));
+        const loginBtn = DOM.querySelector("#loginBtn");
+        // loginBtn.click();
+        expect(loginBtn).toBeTruthy();
     });
 });

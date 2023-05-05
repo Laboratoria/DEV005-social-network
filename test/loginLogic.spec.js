3aa398c438704204034bc9d92e6214289b3fe603
Ericka Gonzales <gonzaleslop03@gmail.com>
2023-05-05 18:23:56 -0500

    ADD: Añadiendo las pruebas de test que estan por corregir

--- test/loginLogic.spec.js
+++ test/loginLogic.spec.js
@@ -2,22 +2,37 @@
  * @jest-8environment ./src/test/my-custom-environment
  */
 import { loginLogic } from "../src/lib/loginLogic.js";
-// import * as configuracion from "../src/firebase/configuracion.js";
+import * as configuracion from "../src/firebase/configuracion.js";
 import { login } from "../src/pages/login.js";
 
 describe("loginLogic", () => {
     test("should be a function", () => {
         expect(typeof loginLogic).toBe("function");
     });
-    // flujo - en el primer test traer cuno de los elementos/existencia, validaciones de texto
+
+    // test("inputs with users data", () => {
+    // const DOM = document.createElement("container");
+    // const mailUser = DOM.querySelector("mailUserLogin");
+    // expect(mailUser).toBeTruthy();
+    // });
+
+    describe("signIn", () => {
+        test("data is collected before the click ", () => {
+            jest.spyOn(configuracion, "signIn").mockImplementation(() => Promise.resolve({ email: "gonzaleslop03@gmail.com", password: "gitanaazu23" }));
+            const DOM = document.createElement("container");
+            DOM.append(login);
+            const email = DOM.querySelector("#mailUserLogin");
+            email.value = "gonzaleslop03@gmail.com";
+            const password = DOM.querySelector("#passwordUserLogin");
+            password.value = "gitanaazul23";
+            expect(DOM).toBeTruthy();
+        });
+    });
+
     test("after click the user navigates to /home", () => {
         const findRouteAndNavigate = jest.fn();
         const DOM = login(findRouteAndNavigate);
-        // const domManipulation = document.createElement("section");
-        // const findAndNavigate = jest.fn();
-        // domManipulation.append((loginLogic(findAndNavigate)));
         const loginBtn = DOM.querySelector("#loginBtn");
-        // loginBtn.click();
         expect(loginBtn).toBeTruthy();
     });
 });

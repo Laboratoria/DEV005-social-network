80c3335bc67a5e0b2450b0033b336cc88df9e682
FRANCISCG97 <f.carrasco.g.97@gmail.com>
2023-05-10 14:26:45 -0400

    Add Delete, Edit and Like buttons in wall.js and post.js, add some functions in post.js, Update style.css

--- src/main.js
+++ src/main.js
@@ -40,8 +40,6 @@ onAuthStateChanged(auth, (currentUser) => {
   console.log(currentUser);
   if (currentUser) {
     navigateTo('/wall');
-    // const user = auth.currentUser;
-    // if (user != )
   } else {
     navigateTo('/');
   }

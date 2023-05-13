3f6b507d8758f2adb571c2d46adf71c60c231379
Ericka Gonzales <gonzaleslop03@gmail.com>
2023-05-13 11:39:25 -0500

    ADD: Iniciando el estilo en /home

--- src/lib/homeLogic.js
+++ src/lib/homeLogic.js
@@ -37,6 +37,7 @@ const homeLogic = (container) => {
             const postElement = document.createElement("DIV");
             // Se crea un botón para eliminar el post, se le agrega un evento click que llama a la función deletePost con el ID del documento como parámetro, y se agrega este botón como hijo del elemento DIV creado en el paso anterior.
             const deleteBtn = document.createElement("img");
+            deleteBtn.classList.add("deleteBtn");
             deleteBtn.src = "./img/iconDelete.svg";
             // deleteBtn.textContent = "Eliminar";
             deleteBtn.addEventListener("click", () => {

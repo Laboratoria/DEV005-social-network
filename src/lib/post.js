05ed52c1dfc3d99d7b1ad87394a0174c475bd76b
FRANCISCG97 <f.carrasco.g.97@gmail.com>
2023-05-15 10:41:43 -0400

    Like and dislike functions were added in post.js and wall.js, and styles were modified in mobile view in style.css

--- src/lib/post.js
+++ src/lib/post.js
@@ -2,13 +2,9 @@
 /* eslint-disable no-shadow */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
-/* eslint-disable no-shadow */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import {
-  collection, addDoc, doc, deleteDoc, updateDoc, arrayUnion, arrayRemove, getDocs, query, onSnapshot, serverTimestamp, orderBy,
+  collection, addDoc, doc, deleteDoc, updateDoc, arrayUnion, arrayRemove, query, onSnapshot, serverTimestamp, orderBy,
 } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { async } from 'regenerator-runtime';
@@ -32,46 +28,16 @@ export const addPostToFirestore = async (text, user) => {
     console.log('Error', err);
   }
 };
-// Función para traer los post desde Firestore
-export const addPostFromFirestore = async () => {
-  const querySnapshot = await getDocs(collection(db, 'posts'));
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, ' => ', doc.data());
-  });
-};
+
 export const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
 
 // Función para eliminar un post de Firestore
 export const deletePostFromFirestore = (id) => deleteDoc(doc(db, 'posts', id));
 
 // Función Firestore para dar Like
-export const likePost = async (doc) => {
-  console.log('función dar like');
-  const userIn = auth.currentUser;
-  try {
-    const postId = doc;
-    const postRef = collection(doc(db, 'posts', postId));
-    console.log(postId.id);
-    const likeArray = await updateDoc(postRef, {
-      likes: arrayUnion(userIn.email),
-    });
-    console.log(likeArray);
-  } catch (err) {
-    console.log('Error', err);
-  }
-};
+export const likePost = (userEmail, idDoc) => updateDoc(doc(db, 'posts', idDoc), { likes: arrayUnion(userEmail) });
+
 // Función Firestore para quitar Like
-export const dislikePost = async () => {
-  console.log('función quitar like');
-  const userIn = auth.currentUser;
-  try {
-    const postRef = doc(db, 'posts', userIn);
-    const dislikeArray = await updateDoc(postRef, {
-      likes: arrayRemove(userIn.email),
-    });
-    console.log(dislikeArray);
-  } catch (err) {
-    console.log('Error', err);
-  }
-};
+export const dislikePost = (userEmail, idDoc) => updateDoc(doc(db, 'posts', idDoc), { likes: arrayRemove(userEmail) });
+
 export { onSnapshot, doc, orderBy };

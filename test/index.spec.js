b668f821fc1a5d8fd42eafd02808c0ec1427d279
Jhoana <jhoanazamora2@gmail.com>
2023-05-16 09:35:03 -0500

    UPDATE: Test de verificación de elementos y funcionalidades en homeLogic

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,7 +8,6 @@ import { homeLogic } from "../src/lib/index.js";
 
 describe("homeLogic", () => {
     test("Debe verificar el contenedor y los elementos HTML necesarios", () => {
-        // create mock container element with necessary HTML elements
         const container = document.createElement("DIV");
         container.innerHTML = `
         <textarea id="commentField"></textarea>
@@ -18,22 +17,17 @@ describe("homeLogic", () => {
         <span id="userName"></span>
       `;
 
-        // create mock user object in localStorage
         const user = { displayName: "John Doe" };
         window.localStorage.setItem("user", JSON.stringify(user));
 
-        // create mock signOut function and attach to auth object
         const signOutMock = jest.fn();
         const signOutSpy = jest.spyOn(auth, "signOut").mockImplementation(signOutMock);
 
-        // mock window.location.href
         delete window.location;
         window.location = { href: "" };
 
-        // call homeLogic function with mock container
         homeLogic(container);
 
-        // verify necessary HTML elements are present and have correct text or values
         expect(container.querySelector("#commentField")).toBeTruthy();
         expect(container.querySelector("#publishBtn")).toBeTruthy();
         expect(container.querySelector("#publications")).toBeTruthy();
@@ -41,14 +35,11 @@ describe("homeLogic", () => {
         expect(container.querySelector("#userName")).toBeTruthy();
         expect(container.querySelector("#userName").textContent).toBe(user.displayName);
 
-        // simulate click on signOffBtn and verify signOut function is called
         container.querySelector("#signOffBtn").click();
         expect(signOutMock).toHaveBeenCalled();
 
-        // verify window.location.href is updated
         expect(window.location.href).toBe("welcome");
 
-        // reset mocks and spies
         signOutSpy.mockRestore();
         window.location = undefined;
     });

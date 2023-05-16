7f715a51c1d44350d941d2a9ba93dc1d1d66b6db
FRANCISCG97 <f.carrasco.g.97@gmail.com>
2023-05-16 15:11:02 -0400

    View style updates in style.css

--- src/components/register.js
+++ src/components/register.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
-/* eslint-disable no-alert */
-
 import { newAccount, accessWithGoogle, accessWithGithub } from '../lib/auth';
 
 // Creación sección registro
@@ -16,6 +12,7 @@ function register(navigateTo) {
   titleReg.id = 'register-title';
   const inputEmail = document.createElement('input');
   const regTrademark = document.createElement('footer');
+  regTrademark.id = 'footer-reg';
   divContainer.id = 'container';
   divContainer.classList.add('signup-container');
   regTrademark.id = 'marca';
@@ -62,6 +59,14 @@ function register(navigateTo) {
   btnRegWithGithub.textContent = 'Regístrate con GitHub';
   regTrademark.textContent = 'KittyBook, 2023';
 
+  // Sección para tablets y dekstop
+  const infoContReg = document.createElement('section');
+  infoContReg.id = 'info-cont-reg';
+  const infoTxtReg = document.createElement('p');
+  infoTxtReg.id = 'info-text-reg';
+  infoTxtReg.textContent = `¿Tienes un gatito? ¡En KittyBook puedes presumirlo!
+  Publica ideas e imágenes para compartir información gatuna.`;
+
   // Creación botón Volver al Inicio
   const btnReturnH = document.createElement('button');
   btnReturnH.id = 'return';
@@ -94,11 +99,14 @@ function register(navigateTo) {
   );
   sectionReg.append(
     titleRegKitty,
+    infoContReg,
     titleReg,
     divContainer,
     btnReturnH,
     regTrademark,
   );
+
+  infoContReg.append(infoTxtReg);
   btnRegWithGoogle.append(signUpGoogleIcon);
   btnRegWithGithub.append(signUpGithubIcon);
   return sectionReg;
